cdc2a5d4b07efb3f0020ded077f319d438e74a80
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-14 16:19:54 -0400

    set routes up to use jobController

---
+++ routes/jobs.js
@@ -0,0 +1,12 @@
+const express = require('express');
+const router = express.Router();
+const jobsController = require('../controlers/jobs')
+
+router.get('/', jobsController.all);
+router.get('/:id', jobsController.show)
+router.get('/new', jobsController.new)
+router.patch('/:id', jobsController.edit)
+router.delete('/:id', jobsController.delete)
+
+
+module.exports = router;
