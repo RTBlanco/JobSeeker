c9618d9799c84416a225901414f1f4622d14c398
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-14 15:53:40 -0400

    added more function for userController

--- controlers/users.js
+++ controlers/users.js
@@ -2,6 +2,22 @@
 const userController = {
   all (req, res) {
     res.send("<h1>Users</h1>")
+  },
+  
+  show (req, res) {
+    res.send(`<h1>route is info for ${req.params.id}</h1>`)
+  }, 
+
+  new (req, res) {
+    res.send('<h1>this route is to create a new user</h1>')
+  },
+
+  edit (req, res) {
+    res.send(`<h1> this route is use to edit the with ${req.params.id}`)
+  },
+
+  delete (req, res) {
+    res.send(`this will delete the use with ${req.params.id}`)
   }
 };
 
