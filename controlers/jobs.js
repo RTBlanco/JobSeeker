d3f7442e47f89217b2a757b9861f474de2d7382b
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-14 17:14:09 -0400

    created the jobController functions

--- controlers/jobs.js
+++ controlers/jobs.js
@@ -1,1 +1,24 @@
-// TODO: create the jobsController
+
+const jobsController = {
+  all (req, res) {
+    res.send("<h1>Gets all Jobs</h1>")
+  },
+  
+  show (req, res) {
+    res.send(`<h1>route is get info for job with id of  ${req.params.id}</h1>`)
+  }, 
+
+  new (req, res) {
+    res.send('<h1>this route is to create a new job</h1>')
+  },
+
+  edit (req, res) {
+    res.send(`<h1> this route to edit the job with id of ${req.params.id}`)
+  },
+
+  delete (req, res) {
+    res.send(`this will delete the job with id of ${req.params.id}`)
+  }
+};
+
+module.exports = jobsController;
