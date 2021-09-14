9e4fdd0a06ecbdd936200acac9c53c57e00f0ab7
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-14 16:01:46 -0400

    updated routes to use controllers

--- routes/users.js
+++ routes/users.js
@@ -2,23 +2,11 @@ const express = require('express');
 const router = express.Router();
 const userController = require('../controlers/users')
 
-/* GET home page. */
 router.get('/', userController.all);
+router.get('/:id', userController.show)
+router.get('/new', userController.new)
+router.patch('/:id', userController.edit)
+router.delete('/:id', userController.delete)
 
-router.get('/:id', (req, res) => {
-  res.send(`route is info for ${req.params.id}`)
-})
-
-router.get('/new', (req, res) => {
-  res.send(`create a new user`)
-})
-
-router.patch('/:id', (req, res) => {
-  res.send(`route is for editing ${req.params.id}`)
-})
-
-router.delete('/:id', (req, res) => {
-  res.send(`router is for deleting ${req.params.id}`)
-})
 
 module.exports = router;
