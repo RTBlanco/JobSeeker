4e32d3eec414bd56e8ef2d76576c8a9b06cb12ba
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-14 15:09:20 -0400

    added routes for testing

--- routes/users.js
+++ routes/users.js
@@ -6,4 +6,20 @@ router.get('/', (req, res) =>  {
   res.send("<h1>Users</h1>");
 });
 
+router.get('/:id', (req, res) => {
+  res.send(`route is info for ${req.params.id}`)
+})
+
+router.get('/new', (req, res) => {
+  res.send(`create a new user`)
+})
+
+router.patch('/:id', (req, res) => {
+  res.send(`route is for editing ${req.params.id}`)
+})
+
+router.delete('/:id', (req, res) => {
+  res.send(`router is for deleting ${req.params.id}`)
+})
+
 module.exports = router;
