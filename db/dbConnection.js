c50c7740d2df66efdabd7b07494097b393b37497
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-15 16:54:47 -0400

    exported db

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,5 +1,19 @@
 const { Sequalize } = require('sequelize');
+
 const sequelize = new Sequelize({
   dialect: 'sqlite',
   storage: './db/database.sqlite'
 });
+
+const checkConnection =async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+}
+
+checkConnection()
+
+module.exports = sequelize
