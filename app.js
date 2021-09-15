7e3c38160abdfa648374bc8581f83b40155910d0
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-15 16:18:09 -0400

    added cors

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express');
 const path = require('path');
 const cookieParser = require('cookie-parser');
 const logger = require('morgan');
+const cors = require('cors');
 
 const sequalize = require('./db/dbConnection');
 
@@ -21,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 app.use(upload.array())
+app.use(cors())
 
 const sync = async () => await sequelize.sync({alter:true})
 sync()
