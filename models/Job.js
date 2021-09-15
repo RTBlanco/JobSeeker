18a545fa1538b6b8a721b0fc1534f440333e4982
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-15 16:51:31 -0400

    created job model

--- models/Job.js
+++ models/Job.js
@@ -1,2 +1,31 @@
-const {Database} = require('sequelize');
+const {DataTypes, Model} = require('sequelize');
 const sequelize = require('../db/dbConnection');
+
+class Job extends Model {
+
+  // Create a method for Interviews
+}
+
+User.init({
+  position: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  companyName: {
+    type: DataTypes.STRING,
+    allowNull: false
+  },
+
+  hopeSal: {
+    type: DataTypes.INTEGER,
+  },
+
+  status: {
+    type: DataTypes.STRING
+  },
+
+  offer: {
+    type: DataTypes.INTEGER
+  }
+})
