3c79d922abdebbbb79b03f4f00649caa1e78392a
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-18 20:59:20 -0400

    created the method names for the interviewController

--- controlers/interviews.js
+++ controlers/interviews.js
@@ -3,7 +3,25 @@ const Interview = require('../models/Interview');
 
 
 const InterviewRouter = {
+  async all(req, res) {
+    
+  },
+   
+  async show(req, res) {
 
+  },
+
+  async new (req, res) {
+
+  },
+
+  async edit (req, res) {
+
+  },
+
+  async delete (req, res) {
+
+  }
 }
 
 module.exports = InterviewRouter;
