6589dd28cdf61efc8e0462f16f9d60e4b5d2a5e8
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-18 22:21:04 -0400

    attached interviewRouter to job routes

--- routes/jobs.js
+++ routes/jobs.js
@@ -1,12 +1,15 @@
 const express = require('express');
 const router = express.Router({mergeParams: true});
 const jobsController = require('../controlers/jobs')
+const interviewRouter = require('./interviews')
 
 router.get('/', jobsController.all);
 router.get('/:id', jobsController.show)
 router.post('/new', jobsController.new)
 router.patch('/:id', jobsController.edit)
 router.delete('/:id', jobsController.delete)
+
+router.use('/:jobId/interviews', interviewRouter)
 
 
 module.exports = router;
