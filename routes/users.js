b26fe52cfc85fa7b91d491ecfc04dc78dec10c14
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-20 12:28:51 -0400

    added user auth to the show route

--- routes/users.js
+++ routes/users.js
@@ -2,10 +2,11 @@ const express = require('express');
 const router = express.Router({mergeParams: true});
 const userController = require('../controlers/users')
 const jobRouter = require('./jobs')
+const {authByToken} = require('../middleware/auth')
 
 router.post('/login', userController.login)
 router.get('/', userController.all);
-router.get('/:id', userController.show)
+router.get('/:id', authByToken, userController.show)
 router.post('/new', userController.new)
 router.patch('/:id', userController.edit)
 router.delete('/:id', userController.delete)
