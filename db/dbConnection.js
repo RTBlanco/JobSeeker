a0a4b01618553330b6a727443605bbecde248255
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-21 16:49:45 -0400

    made changed to the database

--- db/dbConnection.js
+++ db/dbConnection.js
@@ -1,19 +1,19 @@
-const { Sequelize } = require('sequelize');
+// const { Sequelize } = require('sequelize');
 
-const sequelize = new Sequelize({
-  dialect: 'sqlite',
-  storage: './db/database.sqlite'
-});
+// const sequelize = new Sequelize({
+//   dialect: 'sqlite',
+//   storage: './db/database.sqlite'
+// });
 
-const checkConnection = async () => {
-  try {
-    await sequelize.authenticate();
-    console.log('Connection has been established successfully.');
-  } catch (error) {
-    console.error('Unable to connect to the database:', error);
-  }
-}
+// const checkConnection = async () => {
+//   try {
+//     await sequelize.authenticate();
+//     console.log('Connection has been established successfully.');
+//   } catch (error) {
+//     console.error('Unable to connect to the database:', error);
+//   }
+// }
 
-checkConnection()
+// checkConnection()
 
-module.exports = sequelize
+// module.exports = sequelize
