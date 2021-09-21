2b554a958940d885f43c39f10fdd85af52ebc22a
RTBlanco <ronnytoribio1@hotmail.com>
2021-09-21 16:48:47 -0400

    added ability to use migrations

--- app.js
+++ app.js
@@ -17,22 +17,22 @@ const InterviewRouter = require('./routes/interviews');
 const app = express();
 
 // Relations
-const User = require('./models/User');
-const Job = require('./models/Job');
-const Interview = require('./models/Interview');
+// const User = require('./models/User');
+// const Job = require('./models/Job');
+// const Interview = require('./models/Interview');
 
 // User has Many jobs 
-User.hasMany(Job, { onDelete: 'CASCADE' })
-Job.belongsTo(User)
+// User.hasMany(Job, { onDelete: 'CASCADE' })
+// Job.belongsTo(User)
 
-// Jobs has many interviews
-Job.hasMany(Interview, {onDelete: "CASCADE"})
-Interview.belongsTo(Job)
+// // Jobs has many interviews
+// Job.hasMany(Interview, {onDelete: "CASCADE"})
+// Interview.belongsTo(Job)
 
 
 // consider using migrations in the future
-const sync = async () => await sequalize.sync()
-sync()
+// const sync = async () => await sequalize.sync()
+// sync()
 
 app.use(logger('dev'));
 app.use(express.json());
